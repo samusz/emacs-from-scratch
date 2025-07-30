989faac6152f57202a9e6c076165f894c74f093d
Sacha Muszlak <sacha.muszlak@inrae.fr>
2025-07-30 16:44:22 +0200

    Add init.el

---
+++ init.el
@@ -0,0 +1,65 @@
+;; Don't show the splash screen
+(setq inhibit-startup-message t  ; Comment at end of line!
+      visible-bell t) 
+;; Display line numbers in every buffer
+(global-display-line-numbers-mode 1)
+
+;; Theme
+;; Load the Modus Vivendi dark theme
+(load-theme 'modus-vivendi t)
+(setq scroll-step 7)
+
+(set-face-attribute 'default nil :font "FiraCode Nerd Font" :height 160)
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package command-log-mode)
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+
+
+
+
+
+
+
